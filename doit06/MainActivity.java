1ad15f78d5020b8b5ce30cbf7eebfecba9d8a3c7
cryingdryice <104723064+cryingdryice@users.noreply.github.com>
2023-07-21 01:46:55 +0900

    Add files via upload

---
+++ doit06/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.doit06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    int x; // 시크바의 값
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        SeekBar seekBar = findViewById(R.id.seekBar2);
+        textView = findViewById(R.id.textView);
+        progressBar.setProgress(x);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                x = seekBar.getProgress();
+                textView.setText(String.format("선택 값은 %d 입니다.", x));
+                progressBar.setProgress(x);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
